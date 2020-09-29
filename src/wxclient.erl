5287546a95d004f24330f34b0d6336295f42fcdb
Ilaynuriel <ilay5555@gmail.com>
2020-09-29 22:58:10 +0300

    Added ListBox, ComboBox, TextBox...

--- src/wxclient.erl
+++ src/wxclient.erl
@@ -24,20 +24,34 @@ start() ->
   MainSizer = wxBoxSizer:new(?wxVERTICAL),
   SubSizer1 = wxBoxSizer:new(?wxVERTICAL),
   SubSizer2 = wxBoxSizer:new(?wxVERTICAL),
+%%  ListBoxSizer = wxStaticBoxSizer:new(?wxVERTICAL, SubSizer1, [{label, "wxListBox"}]),
 
   TopTxt = wxStaticText:new(Frame, ?wxID_ANY, "Query Window"),
   Headline = wxStaticText:new(Frame, ?wxID_ANY, "Insert Value:"),
   TextCtrl  = wxTextCtrl:new(Frame, 1, [{value, ""}, {style, ?wxDEFAULT}]),
 
+  Choices = ["Name","Year","Description", "Actor","Budget"],
+
+  %% Create a wxListBox that uses multiple selection
+  ListBox = wxListBox:new(Frame, 1, [{size, {-1,100}},
+    {choices, Choices}, {style, ?wxLB_SINGLE}]), %%TODO: cancel multiple selection
+  wxListBox:setToolTip(ListBox, "A wxListBox with multiple selection"),
+
+  ComboBox = wxComboBox:new(Frame, 5, [{choices, Choices}]),
+  wxComboBox:setToolTip(ComboBox, "A wxComboBox"),
+
   wxSizer:add(SubSizer1, TopTxt, [{flag, ?wxALL bor ?wxEXPAND}, {border, 8}]),
   wxSizer:add(SubSizer1, Headline, [{flag, ?wxALL bor ?wxEXPAND}, {border, 8}]),
   wxSizer:add(SubSizer1, TextCtrl, [{flag, ?wxALL bor ?wxEXPAND}, {border, 8}]),
+  wxSizer:add(SubSizer2, ListBox, [{flag, ?wxALL bor ?wxEXPAND}, {border, 8}]),
+  wxSizer:add(SubSizer2, ComboBox, [{flag, ?wxALL bor ?wxEXPAND}, {border, 8}]),
 
   Font = wxFont:new(14, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?wxFONTWEIGHT_NORMAL,[{underlined, true}]),
-%%  wxFont:setUnderlined(Font,true),
   wxTextCtrl:setFont(TopTxt, Font),
   wxSizer:add(MainSizer, SubSizer1, [{flag, ?wxALIGN_LEFT}, {border, 5}]),
   wxSizer:add(MainSizer, SubSizer2, [{flag, ?wxALIGN_RIGHT}, {border, 5}]),
+
+
 
   wxWindow:setSizer(Frame, MainSizer),
   wxFrame:show(Frame).
