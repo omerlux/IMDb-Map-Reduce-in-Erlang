22bac5b492f2a0b75e884694e5d595be678308da
omerlux <omerlux1994@gmail.com>
2020-10-05 21:16:01 +0300

    - Fixed score again - now it's avg_vote

--- src/mapreduce.erl
+++ src/mapreduce.erl
@@ -71,15 +71,15 @@ map(Table, Query = #query{type = generic, searchVal = SearchVal}) ->
         [Movie || {_id, Movie} <- ets:tab2list(Table), string:str(Movie#movie_data.description, SearchVal) > 0];
       % note: range of +-0.5
       "Score" ->
-        case string:str(SearchVal, ".") > 0 of
+      case string:str(SearchVal, ".") > 0 of
           true -> % it's a float
             [Movie || {_id, Movie} <- ets:tab2list(Table),
-              Movie#movie_data.metascore > float_to_list(list_to_float(SearchVal) - 0.5),
-              Movie#movie_data.metascore < float_to_list(list_to_float(SearchVal) + 0.5)];
+              Movie#movie_data.avg_vote > float_to_list(list_to_float(SearchVal) - 0.5),
+              Movie#movie_data.avg_vote < float_to_list(list_to_float(SearchVal) + 0.5)];
           false -> % it's an integer
             [Movie || {_id, Movie} <- ets:tab2list(Table),
-              Movie#movie_data.metascore > integer_to_list(list_to_integer(SearchVal) - 0.5),
-              Movie#movie_data.metascore < integer_to_list(list_to_integer(SearchVal) + 0.5)]
+              Movie#movie_data.avg_vote > integer_to_list(list_to_integer(SearchVal) - 0.5),
+              Movie#movie_data.avg_vote < integer_to_list(list_to_integer(SearchVal) + 0.5)]
         end;
       "Budget" ->
         [Movie || {_id, Movie} <- ets:tab2list(Table), string:str(Movie#movie_data.budget, SearchVal) > 0];
