1a90fa7abd87eb8b2a11b3a50e8c65083d5f8d25
omerlux <omerlux1994@gmail.com>
2020-10-05 19:25:52 +0300

    Added automatically distribution to alive servers (also works with disconnecting from one).

--- src/mapreduce.erl
+++ src/mapreduce.erl
@@ -87,84 +87,131 @@ map(_Table, _Query = #query{}) ->
 % generic reduce - taking only the resultCategory.
 reduce(MappedList, Query = #query{type = generic}) ->
   %% each element in list will be construct fromt he query.resultCategory section
-  [#movie_data{id = Movie#movie_data.id, title = Movie#movie_data.title,
-    original_title = (fun() -> case Query#query.resultCategory#movie_data.original_title == true of
-                                 true -> Movie#movie_data.original_title;
-                                 false -> ""
-                               end end),
-    year = (fun() -> case Query#query.resultCategory#movie_data.year == true of
-                       true -> Movie#movie_data.year;
-                       false -> ""
-                     end end),
-    date_published = (fun() -> case Query#query.resultCategory#movie_data.date_published == true of
-                                 true -> Movie#movie_data.date_published;
-                                 false -> ""
-                               end end),
-    genre = (fun() -> case Query#query.resultCategory#movie_data.genre == true of
-                        true -> Movie#movie_data.genre;
-                        false -> ""
-                      end end),
-    duration = (fun() -> case Query#query.resultCategory#movie_data.duration == true of
-                           true -> Movie#movie_data.duration;
-                           false -> ""
-                         end end),
-    country = (fun() -> case Query#query.resultCategory#movie_data.country == true of
-                          true -> Movie#movie_data.country;
-                          false -> ""
-                        end end),
-    language = (fun() -> case Query#query.resultCategory#movie_data.language == true of
-                           true -> Movie#movie_data.language;
-                           false -> ""
-                         end end),
-    director = (fun() -> case Query#query.resultCategory#movie_data.director == true of
-                           true -> Movie#movie_data.director;
-                           false -> ""
-                         end end),
-    writer = (fun() -> case Query#query.resultCategory#movie_data.writer == true of
-                         true -> Movie#movie_data.writer;
-                         false -> ""
-                       end end),
-    production_company = (fun() -> case Query#query.resultCategory#movie_data.production_company == true of
-                                     true -> Movie#movie_data.production_company;
-                                     false -> ""
-                                   end end),
-    actors = (fun() -> case Query#query.resultCategory#movie_data.actors == true of
-                         true -> Movie#movie_data.actors;
-                         false -> ""
-                       end end),
-    avg_vote = (fun() -> case Query#query.resultCategory#movie_data.avg_vote == true of
-                           true -> Movie#movie_data.avg_vote;
-                           false -> ""
-                         end end),
-    votes = (fun() -> case Query#query.resultCategory#movie_data.votes == true of
-                        true -> Movie#movie_data.votes;
-                        false -> ""
-                      end end),
-    budget = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
-                         true -> Movie#movie_data.budget;
-                         false -> ""
-                       end end),
-    usa_gross_income = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
-                                   true -> Movie#movie_data.budget;
-                                   false -> ""
-                                 end end),
-    worlwide_gross_income = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
-                                        true -> Movie#movie_data.budget;
-                                        false -> ""
-                                      end end),
-    metascore = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
-                            true -> Movie#movie_data.budget;
-                            false -> ""
-                          end end),
-    reviews_from_users = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
-                                     true -> Movie#movie_data.budget;
-                                     false -> ""
-                                   end end),
-    reviews_from_critics = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
-                                       true -> Movie#movie_data.budget;
-                                       false -> ""
-                                     end end)}
-    || Movie = #movie_data{} <- MappedList].
+
+  % note: after success try, delete it
+%%  Fun_value = fun(Category, Condition, Value) ->
+%%    case Condition of
+%%      true -> Value;
+%%      false -> ""
+%%    end
+%%    end,
+%%  Fun_moviedata_reduce = fun(Hey = #hey{}) ->
+%%    Hey_ans = #hey{a = Fun_value(a, false, Hey#hey.a),
+%%      b = Fun_value(b, true, Hey#hey.b),
+%%      c = Fun_value(c, false, Hey#hey.c)}
+%%                         end,
+  %% mapping the wanted values -
+  % Condition is the resultCategory#movie_data.CATEGORY value
+  % Value is the value we want to insert the table
+  Fun_value = fun(Condition, Value) ->
+    case Condition of
+      true -> Value;
+      false -> ""
+    end
+              end,
+  Fun_moviedata_reduce = fun(Movie = #movie_data{}, Query) ->
+    #movie_data{id = Movie#movie_data.id, title = Movie#movie_data.title,
+      original_title = Fun_value(Query#query.resultCategory#movie_data.original_title, Movie#movie_data.original_title),
+      year = Fun_value(Query#query.resultCategory#movie_data.year, Movie#movie_data.year),
+      date_published = Fun_value(Query#query.resultCategory#movie_data.date_published, Movie#movie_data.date_published),
+      genre = Fun_value(Query#query.resultCategory#movie_data.genre, Movie#movie_data.genre),
+      duration = Fun_value(Query#query.resultCategory#movie_data.duration, Movie#movie_data.duration),
+      country = Fun_value(Query#query.resultCategory#movie_data.country, Movie#movie_data.country),
+      language = Fun_value(Query#query.resultCategory#movie_data.language, Movie#movie_data.language),
+      director = Fun_value(Query#query.resultCategory#movie_data.director, Movie#movie_data.director),
+      writer = Fun_value(Query#query.resultCategory#movie_data.writer, Movie#movie_data.writer),
+      production_company = Fun_value(Query#query.resultCategory#movie_data.production_company, Movie#movie_data.production_company),
+      actors = Fun_value(Query#query.resultCategory#movie_data.actors, Movie#movie_data.actors),
+      avg_vote = Fun_value(Query#query.resultCategory#movie_data.avg_vote, Movie#movie_data.avg_vote),
+      votes = Fun_value(Query#query.resultCategory#movie_data.votes, Movie#movie_data.votes),
+      budget = Fun_value(Query#query.resultCategory#movie_data.budget, Movie#movie_data.budget),
+      usa_gross_income = Fun_value(Query#query.resultCategory#movie_data.usa_gross_income, Movie#movie_data.usa_gross_income),
+      worlwide_gross_income = Fun_value(Query#query.resultCategory#movie_data.worlwide_gross_income, Movie#movie_data.worlwide_gross_income),
+      metascore = Fun_value(Query#query.resultCategory#movie_data.metascore, Movie#movie_data.metascore),
+      reviews_from_users = Fun_value(Query#query.resultCategory#movie_data.reviews_from_users, Movie#movie_data.reviews_from_users),
+      reviews_from_critics = Fun_value(Query#query.resultCategory#movie_data.reviews_from_critics, Movie#movie_data.reviews_from_critics)}
+                         end,
+
+  [Fun_moviedata_reduce(Movie) || Movie = #movie_data{} <- MappedList].
+
+%%  [#movie_data{id = Movie#movie_data.id, title = Movie#movie_data.title,
+%%    original_title = (fun() -> case Query#query.resultCategory#movie_data.original_title == true of
+%%                                 true -> Movie#movie_data.original_title;
+%%                                 false -> ""
+%%                               end end),
+%%    year = (fun() -> case Query#query.resultCategory#movie_data.year == true of
+%%                       true -> Movie#movie_data.year;
+%%                       false -> ""
+%%                     end end),
+%%    date_published = (fun() -> case Query#query.resultCategory#movie_data.date_published == true of
+%%                                 true -> Movie#movie_data.date_published;
+%%                                 false -> ""
+%%                               end end),
+%%    genre = (fun() -> case Query#query.resultCategory#movie_data.genre == true of
+%%                        true -> Movie#movie_data.genre;
+%%                        false -> ""
+%%                      end end),
+%%    duration = (fun() -> case Query#query.resultCategory#movie_data.duration == true of
+%%                           true -> Movie#movie_data.duration;
+%%                           false -> ""
+%%                         end end),
+%%    country = (fun() -> case Query#query.resultCategory#movie_data.country == true of
+%%                          true -> Movie#movie_data.country;
+%%                          false -> ""
+%%                        end end),
+%%    language = (fun() -> case Query#query.resultCategory#movie_data.language == true of
+%%                           true -> Movie#movie_data.language;
+%%                           false -> ""
+%%                         end end),
+%%    director = (fun() -> case Query#query.resultCategory#movie_data.director == true of
+%%                           true -> Movie#movie_data.director;
+%%                           false -> ""
+%%                         end end),
+%%    writer = (fun() -> case Query#query.resultCategory#movie_data.writer == true of
+%%                         true -> Movie#movie_data.writer;
+%%                         false -> ""
+%%                       end end),
+%%    production_company = (fun() -> case Query#query.resultCategory#movie_data.production_company == true of
+%%                                     true -> Movie#movie_data.production_company;
+%%                                     false -> ""
+%%                                   end end),
+%%    actors = (fun() -> case Query#query.resultCategory#movie_data.actors == true of
+%%                         true -> Movie#movie_data.actors;
+%%                         false -> ""
+%%                       end end),
+%%    avg_vote = (fun() -> case Query#query.resultCategory#movie_data.avg_vote == true of
+%%                           true -> Movie#movie_data.avg_vote;
+%%                           false -> ""
+%%                         end end),
+%%    votes = (fun() -> case Query#query.resultCategory#movie_data.votes == true of
+%%                        true -> Movie#movie_data.votes;
+%%                        false -> ""
+%%                      end end),
+%%    budget = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
+%%                         true -> Movie#movie_data.budget;
+%%                         false -> ""
+%%                       end end),
+%%    usa_gross_income = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
+%%                                   true -> Movie#movie_data.budget;
+%%                                   false -> ""
+%%                                 end end),
+%%    worlwide_gross_income = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
+%%                                        true -> Movie#movie_data.budget;
+%%                                        false -> ""
+%%                                      end end),
+%%    metascore = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
+%%                            true -> Movie#movie_data.budget;
+%%                            false -> ""
+%%                          end end),
+%%    reviews_from_users = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
+%%                                     true -> Movie#movie_data.budget;
+%%                                     false -> ""
+%%                                   end end),
+%%    reviews_from_critics = (fun() -> case Query#query.resultCategory#movie_data.budget == true of
+%%                                       true -> Movie#movie_data.budget;
+%%                                       false -> ""
+%%                                     end end)}
+%%    || Movie = #movie_data{} <- MappedList].
 
 % TODO: delete it if the gui works well...
 %%  case Query#query.resultCategory of
